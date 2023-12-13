5e35bbed2bd452b17a5459b40ec8fbd7a7a3931f
Rayancoder21 <rayan.h.khogali@gmail.com>
2023-12-12 21:43:48 -0800

    shell

--- Main.h
+++ Main.h
@@ -9,7 +9,7 @@
 void _rere_printf_shellllo(const char *my_rere);
 void rere_infinite_prompt(void);
 void re_re_input(char *rere, size_t size);
-void rere_executeCommand(const char *rere);
+void execute_command(const char *command);
 int main(void);
 
 #endif
--- rere_execution.c
+++ rere_execution.c
@@ -1,41 +1,37 @@
 #include "Main.h"
-#include <sys/wait.h>
-#include <sys/types.h>
-#define EXIT_COMMAND "exit"
-
 /**
-*rere_executeCommand - function to execute command
-*@rere: parameter
-*Return: nothing
-*/
+ * execute_command - function
+ * @command: parameter
+ *
+ * Return: nothing
+ */
 
-void rere_executeCommand(const char *rere)
+
+
+void execute_command(const char *command)
 {
-	pid_t pid = fork();
+	pid_t child_pid = fork();
 
-	if (strcmp(rere, EXIT_COMMAND) == 0)
-	{
-		exit(EXIT_SUCCESS);
-	}
-	if (pid == 0)
-	{
-		int argCountRere = 0;
-		char *args[200];
-		char *token_Arg = strtok((char *)rere, " ");
-
-		while (token_Arg != NULL)
-		{
-			args[argCountRere++] = token_Arg;
-			token_Arg = strtok(NULL, " ");
-		}
-		args[argCountRere] = NULL;
-		execvp(args[0], args);
-		_rere_printf_shellllo(" No such file or directory\n");
-		exit(EXIT_FAILURE);
-	}
-	else if (pid == -1)
+	if (child_pid == -1)
 	{
 		_rere_printf_shellllo("Error forking process.\n");
+		exit(EXIT_FAILURE);
+	}
+	else if (child_pid == 0)
+	{
+		char *args[128];
+		int arg_count = 0;
+
+		char *token = strtok((char *)command, " ");
+
+		while (token != NULL)
+		{
+			args[arg_count++] = token;
+			token = strtok(NULL, " ");
+		}
+		args[arg_count] = NULL;
+		execvp(args[0], args);
+		_rere_printf_shellllo("Error executing command.\n");
 		exit(EXIT_FAILURE);
 	}
 	else
